c3b8a198107aa06ae3ad8ead8f0785f743c4bfb7
Vic Vijayakumar <needcaffeine@gmail.com>
2020-11-30 13:29:34 -0500

    refac: update with new eslint options

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,21 @@
 import React from 'react'
 
-function Index() {
-    const allCarsInInventory = [
-        'Honda Accord',
-        'Toyota Corolla',
-        'Toyota Camry',
-        'Subaru Outback',
-        'Tesla Model X',
-    ]
+export default class Index extends React.Component {
+    render() {
+        const allCarsInInventory = ['Honda Accord', 'Toyota Corolla', 'Toyota Camry', 'Subaru Outback', 'Tesla Model X']
 
-    const listOfAvailableCars = allCarsInInventory.map((car, i) => {
-        return <li key={i}>{car}</li>
-    })
+        const listOfAvailableCars = allCarsInInventory.map((car, i) => {
+            return (<li key={i}>{car}</li>)
+        })
 
-    return (
-        <>
-            <h1>Welcome to our car dealership! Here is a list of cars available to test drive:</h1>
+        return (
+            <>
+                <h1>
+                    Welcome to our car dealership! Here is a list of cars available to test drive:
+                </h1>
 
-            <ul>{listOfAvailableCars}</ul>
-        </>
-    )
+                <ul>{listOfAvailableCars}</ul>
+            </>
+        )
+    }
 }
-
-export default Index
